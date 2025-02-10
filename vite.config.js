0b3a2f879ceab63b0892bbcc97eed0148cb3292c
Tsehay21 <tsehay1900@yahoo.com>
2025-02-10 17:24:27 -0500

    username

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Netflix-2025-Clone/",
+  base: "https://Tsehay21.github.io/Netflix-2025-Clone/",
   plugins: [react()],
+});
 
-})
+
