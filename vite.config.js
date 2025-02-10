9278f05037480aa7a786b94309dc38c634842416
Tsehay21 <tsehay1900@yahoo.com>
2025-02-10 16:52:39 -0500

    base

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"/Netflix-2025-Clone/",
+  base: "/Netflix-2025-Clone/",
   plugins: [react()],
 
 })
